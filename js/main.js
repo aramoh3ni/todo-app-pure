6431beffde1fc224f4fb997692ce6690ae41878a
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2023-01-27 22:12:07 +0430

    creating Card Component and adding toggle behavior

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+"use strict";
+
 const sidebarBtn = document.querySelector(".sidebar-toggle");
+
+document.querySelector(".card-action-btn").addEventListener("click", (e) => {
+  document.querySelector(".card-body").classList.toggle("hidden");
+});
 
 sidebarBtn.addEventListener("click", (e) => {
   document.querySelector(".sidebar").classList.toggle("hidden");
-  document.querySelector(".container").classList.toggle("center");
 });
 
 document.querySelector(".body").addEventListener("click", () => {
