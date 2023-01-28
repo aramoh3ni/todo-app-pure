002d0cb1f16eee0134b262f590c111d220c51a0d
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2023-01-28 14:14:09 +0430

    adding card toggle button behavior

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 const sidebarToggleBtn = document.querySelector("#sidebar-toggle-btn");
 const cardActionBtns = document.querySelectorAll("button.card-toggle-btn");
+const cardActionIcons = document.querySelectorAll("button.card-toggle-btn.fa");
 const cardBody = document.querySelectorAll("div.card-body");
 
 sidebarToggleBtn.addEventListener("click", () => {
@@ -18,5 +19,15 @@ const x = document.querySelector("main").style.width;
 cardActionBtns.forEach((item, i) =>
   item.addEventListener("click", (e) => {
     cardBody[i].classList.toggle("hidden");
+
+    if (e.path[0].classList.contains("fa-angle-up")) {
+      e.path[0].classList.remove("fa-angle-up");
+      e.path[0].classList.add("fa-angle-down");
+    } else if (e.path[0].classList.contains("fa-angle-down")) {
+      e.path[0].classList.remove("fa-angle-down");
+      e.path[0].classList.add("fa-angle-up");
+    }
   })
 );
+
+console.log(cardActionIcons.length);
