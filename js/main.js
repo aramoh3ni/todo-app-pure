d2f34b4e77737d4f2a34f6b3fa6a4e433807ad56
Alireza Mohseni <alireza.mohseni.se@gmail.com>
2023-01-29 19:13:43 +0430

    adding new feature click effect for all buttons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const btnEffect = document.querySelectorAll(".btn");
 const sidebarToggleBtn = document.querySelector("#sidebar-toggle-btn");
 const cardOptionBtns = document.querySelectorAll("button.card-option-btn");
 const cardToggleBtns = document.querySelectorAll("button.card-toggle-btn");
@@ -37,3 +38,21 @@ cardOptionBtns.forEach((item, i) => {
     cardOptions[i].classList.toggle("hidden");
   });
 });
+
+// Button Effect Style
+btnEffect.forEach((item) =>
+  item.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    e.currentTarget.style.transform = "scaleX(.98)";
+    e.currentTarget.style.boxShadow = "0px 0px 0px 0px red";
+  })
+);
+
+btnEffect.forEach((item) =>
+  item.addEventListener("mouseup", (e) => {
+    e.preventDefault();
+    e.currentTarget.style.transform = "scaleX(1)";
+    e.currentTarget.style.boxShadow =
+      ".9px .9px .9px .9px rgba(51, 51, 51, 0.005)";
+  })
+);
